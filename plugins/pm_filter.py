de15693560ea47553b1ca4e3482a44220e74431c
DeEzUzZa-7012 <108201261+DeEzUzZa-7012@users.noreply.github.com>
2022-07-25 11:05:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -83,10 +83,10 @@ async def filter(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>🗂️Total File :- {message.from_user.mention}</b>\n<b>🎥Movie Name :-</b> <code>{search}</code> ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🌀 Hey :- {message.from_user.mention}</b>\n<b>🎥Movie Name :-</b> <code>{search}</code> ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text(f"<b>🗂️Total File :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code> ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>🌀 Hey :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code> ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -102,9 +102,9 @@ async def filter(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>🗂️Total File :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code>  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>🌀 Hey :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code>  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>🗂️Total File :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code> ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>🌀 Hey :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code> ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
@@ -149,9 +149,9 @@ async def group(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>🗂️Total File :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code>  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🌀 Hey :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code>  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             else:
-                await message.reply_text(f"<b>🗂️Total File :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code>‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>🌀 Hey :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code>‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -167,9 +167,9 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"­<b>🗂️Total File :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code>  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"­<b>🌀 Hey :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code>  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>­🗂️Total File :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code> ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>­🌀 Hey :- {message.from_user.mention} </b>\n<b>🎥Movie Name :-</b> <code>{search}</code> ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
     
 def get_size(size):
